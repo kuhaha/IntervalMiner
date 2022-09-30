1f7d9002443d4b0687319aab24838b32219c1a55
kuhaha <kevin.yr.cheng@gmail.com>
2022-09-30 11:26:47 +0900

    Update SequenceHandler.java

--- SequenceHandler.java
+++ SequenceHandler.java
@@ -5,6 +5,8 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import ca.pfv.spmf.patterns.itemset_list_integers_without_support.Itemset;
+
 /**
  * 
  * @author kuhaha
@@ -15,10 +17,11 @@ public class SequenceHandler {
 	//raw sequence database: list of interval sequence <start,finish, symbol>
 	protected static List<STI[]> sti_sequences = new ArrayList<STI[]>();
 	
-	//mapping each symbol to a list of its occurrences, 
+	//mapping each symbol to a list of occurrences, 
 	// an occurrence is a map from seq_id to the position in sti_sequence
 	protected static Map<Integer,Map<Integer, Integer>> occurrences = new HashMap<>();
 	
+	// add an occurrence of symbol `sym` at position `pos` in current sti sequence 
 	public static void addOccurrence(int sym, int pos) {
 		int seq = sti_sequences.size();
 		if (!occurrences.containsKey(sym)) {
@@ -29,6 +32,13 @@ public class SequenceHandler {
 			occ.put(seq, pos);
 		}
 	}
+	
+	// add a STI sequence 
+	public static void addSequence(STI[] sti_sequence) {
+		sti_sequences.add(sti_sequence);
+	}
+	
+	// get the STI with symbol `sym` occurring in a specofoc sequence `seq`    
 	public static STI getSTI(int sym, int seq) {
 		if (occurrences.containsKey(sym)
 			&& occurrences.get(sym).containsKey(seq)) 
@@ -38,14 +48,25 @@ public class SequenceHandler {
 		}
 		return null;
 	}
-	public static void addSequence(STI[] sti_sequence) {
-		sti_sequences.add(sti_sequence);
-	}
 	
+	// get the STI with symbol `sym` occurring in a specofoc sequence `seq`    
+	public static String getRelation(SequentialPattern pattern) {
+		StringBuilder r = new StringBuilder("");
+		List<STI> sti_sequence = new ArrayList<STI>();
+		for(Itemset itemset : pattern.getItemsets()){
+			// For each item in the current itemset
+			for(Integer sym : itemset.getItems()){
+				
+			}
+		}
+		return r.toString();
+	}
+
 	public static void print() {
 		System.out.println("=========  STI SEQUENCE DATABASE ==========");
 		System.out.println(ToString());
 	}
+	
 	private static String ToString(){
 		StringBuilder buffer = new StringBuilder();
 		// for each sequence
